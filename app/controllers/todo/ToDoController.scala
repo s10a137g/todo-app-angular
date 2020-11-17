a430466c1c0478edd4446056b44778dec008eb3d
Shinya Morikawa <s10a137g@gmail.com>
2020-11-17 12:38:24 +0900

    Add todo insert display

--- app/controllers/todo/ToDoController.scala
+++ app/controllers/todo/ToDoController.scala
@@ -20,8 +20,16 @@ import scala.concurrent.{Await,Future}
 
 import scala.concurrent.duration._
 
+import play.api.data._
+import play.api.data.Forms._
+
+import play.api.i18n.I18nSupport
+
+// TODO追加フォーム
+case class TodoFormData(title: String, body: String, status: Int, category: Int)
+
 @Singleton
-class ToDoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class ToDoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
 
   def list() = Action { implicit req =>
     
@@ -33,5 +41,27 @@ class ToDoController @Inject()(val controllerComponents: ControllerComponents) e
 
     val todoList  = Await.result(TodoRepository.getAll(), Duration.Inf)
     Ok(views.html.todo.list(vv, todoList))
+  }
+
+  def displayInsert() = Action { implicit req =>
+    val todoForm = Form(
+      mapping(
+          "title"  -> text, 
+          "body"   -> text, 
+          "status" -> number, 
+          "category" -> number
+        )(TodoFormData.apply)(TodoFormData.unapply)
+      )
+    val vv = ViewValueHome(
+      title  = "Todo追加画面",
+      cssSrc = Seq("main.css"),
+      jsSrc  = Seq("main.js")
+    )
+
+    Ok(views.html.todo.insert(vv, todoForm))
+  }
+    
+  def insert() = Action { implicit req =>
+    NoContent
   }
 }
